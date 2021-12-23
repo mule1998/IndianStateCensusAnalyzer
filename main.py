1fab0bbc881168382ee390364dc2190c4484777b
Shubham Mule <shubhambmule@gmail.com>
2021-12-23 07:23:01 +0530

    Added: data from the csv file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import panda as pd
 
 def census():
+    '''
+    Adding data from csv file
+    '''
     csv=pd.read_csv('C:/Users/Shubham/Desktop/IndianCensusAnalyzer/IndianCensus - Sheet1')
     print(csv)
 
